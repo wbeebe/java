c71daee5690b5f7e580335d519b25c1beef11c5d
Bill Beebe <wbeebe@gmail.com>
2018-08-01 13:10:49 -0400

    Move project to Java 10.

---
+++ BorderPaneExample/src/module-info.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+module BorderPaneExample {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.deploy;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+}
