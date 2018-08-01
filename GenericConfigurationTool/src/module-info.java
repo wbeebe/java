600fa42ddaf317b5eec9ef31fd4ed76ed6fc619e
Bill Beebe <wbeebe@gmail.com>
2018-08-01 13:11:31 -0400

    Move project to Java 10.

---
+++ GenericConfigurationTool/src/module-info.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+module GenericConfigurationTool {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.deploy;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires java.logging;
+    requires java.management;
+    requires jdk.management;
+}
