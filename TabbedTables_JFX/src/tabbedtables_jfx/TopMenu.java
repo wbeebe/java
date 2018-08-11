7ceb19bc1404451bbe1c42168e981709a32089b1
Bill Beebe <wbeebe@users.noreply.github.com>
2018-08-11 09:38:50 -0400

    Refactored menu bar to implemented usage of MenuAction interface.

--- TabbedTables_JFX/src/tabbedtables_jfx/TopMenu.java
+++ TabbedTables_JFX/src/tabbedtables_jfx/TopMenu.java
@@ -27,29 +27,102 @@ import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 /**
- *
+ * The application's top level menu bar. All actions and menus are instantiated
+ * here. Default actions for everything are created, and there are helper
+ * functions to set them to something custom based on initial need or context
+ * changes.
+ * 
+ * The getters and setters for MenuAction are for (possible) context setting.
+ * For example, the Exit menu action might be changed to reflect internal changes
+ * that might need to be performed before the application fully exits. You can
+ * always build in logic to test if something changed before exiting within a
+ * single exit procedure, but then you'd have to have access to all application
+ * resources. Replaceable MenuActions are for trying to have as much separation
+ * as reasonably possible.
+ * 
  * @author wbeebe
  */
 public class TopMenu extends MenuBar {
+    Stage stage;
+    MenuAction exitAction;
+    MenuAction aboutAction;
+
     public TopMenu(Stage stage) {
+        this.stage = stage;
+        this.exitAction = new DefaultExitAction();
+        this.aboutAction = new DefaultAboutAction(this.stage);
+
         Menu fileMenu = new Menu("File");
         MenuItem exitMenuItem = new MenuItem("Exit");
         exitMenuItem.setAccelerator(new KeyCodeCombination(KeyCode.F4, KeyCombination.ALT_DOWN));
-        exitMenuItem.setOnAction((ActionEvent ae) -> {
-            Platform.exit();
-        });
+        exitMenuItem.setOnAction((ActionEvent ae) -> { exitAction.doAction(); });
         fileMenu.getItems().addAll(new SeparatorMenuItem(), exitMenuItem);
 
         Menu helpMenu = new Menu("Help");
         MenuItem aboutMenuItem = new MenuItem("About");
-        aboutMenuItem.setOnAction((ActionEvent ae) -> {
-            new AboutDialog(
-                stage,
-                "About Tabbed Tables",
-                "Tabbed Tables V 0.1").showAndWait();
-        });
+        aboutMenuItem.setOnAction((ActionEvent ae) -> { aboutAction.doAction(); });
         helpMenu.getItems().addAll(new SeparatorMenuItem(), aboutMenuItem);
 
         getMenus().addAll(fileMenu, helpMenu);
+    }
+
+    /**
+     * Set the Exit menu action to do an action when invoked.
+     * 
+     * @param action - Custom Exit action instance derived from MenuAction.
+     */
+    public void setExitAction(MenuAction action) {
+        if (action != null) {
+            this.exitAction = action;
+        }
+    }
+
+    public MenuAction getExitAction() { return this.exitAction; }
+
+    /**
+     * Set the About menu action to do an action when invoked.
+     * 
+     * @param action - Custom About action instance derived from MenuAction.
+     */
+    public void setAboutAction(MenuAction action) {
+        if (action != null) {
+            this.aboutAction = action;
+        }
+    }
+
+    public MenuAction getAboutAction() { return this.aboutAction; }
+}
+
+/**
+ * Default main menu exit action. Calls Platform Exit to exit application.
+ * 
+ * @author wbeebe
+ */
+class DefaultExitAction implements MenuAction {
+    @Override
+    public void doAction() {
+        Platform.exit();
+    }
+}
+
+/**
+ * Default main menu About action. A wrapper around invoking an instance of
+ * AboutDialog.
+ * 
+ * @author wbeebe
+ */
+class DefaultAboutAction implements MenuAction {
+    Stage stage;
+
+    public DefaultAboutAction(Stage st) {
+        this.stage = st;
+
+    }
+
+    @Override
+    public void doAction() {
+        var dialog = 
+            new AboutDialog(stage, "About Tabbed Tables", "Tabbed Tables V 0.1");
+        dialog.showAndWait();
     }
 }
